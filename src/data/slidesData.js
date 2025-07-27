2bce488c522154d470b9272712383afa373a595c
Aditya Gaidhani <67220592+Adii-iii@users.noreply.github.com>
2025-07-28 02:40:32 +0530

    replacing the source of images

--- src/data/slidesData.js
+++ src/data/slidesData.js
@@ -23,34 +23,34 @@ const slidesData = [
     background: "#1e293b",
     items: [
       {
-        label: "Hackathon",
-        images: [collage+"IMG_20220714_152242.jpg", collage+"PXL_20230609_142127148.jpg", collage+"IMG_20220714_224332.jpg"],
-        description: "Our first sleepless hackathon. We survived on coffee and adrenaline."
+        label: "oct 2023",
+        images: [collage+"oct 2023.jpg"],
+        // description: "Our first sleepless hackathon. We survived on coffee and adrenaline."
       },
       {
-        label: "Band Practice",
-        images: [collage+"band1.jpg", collage+"band2.jpg", collage+"band3.jpg"],
-        description: "Late-night jamming and chaotic tuning sessions."
+        label: "mar 2024",
+        images: [collage+"mar 2024.jpg"],
+        // description: "Late-night jamming and chaotic tuning sessions."
       },
       {
-        label: "Exam Week",
-        images: [collage+"study1.jpg", collage+"study2.jpg", collage+"study3.jpg"],
-        description: "You explaining DSA while everyone else panicked!"
+        label: "nov 2024",
+        images: [collage+"nov 2024.jpg"],
+        // description: "You explaining DSA while everyone else panicked!"
       },
       {
-        label: "Cultural Fest",
-        images: [collage+"cult1.jpg", collage+"cult2.jpg"],
-        description: "Your solo on the bass shook the crowd."
+        label: "dec 2024",
+        images: [collage+"dec 2024.jpg"],
+        // description: "Your solo on the bass shook the crowd."
       },
       {
-        label: "Club Moments",
-        images: [collage+"club1.jpg"],
-        description: "Behind the scenes of every event you led."
+        label: "mar 2025",
+        images: [collage+"mar 2025.jpg"],
+        // description: "Behind the scenes of every event you led."
       },
       {
-        label: "Last Day",
-        images: [collage+"last1.jpg", collage+"last2.jpg"],
-        description: "Captured emotions, smiles, and a few tears."
+        label: "apr 2025",
+        images: [collage+"april 2025.jpg"],
+        // description: "Captured emotions, smiles, and a few tears."
       }
     ]
   },
